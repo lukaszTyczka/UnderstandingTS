fe4561084bbdf7bc412a2813980428d9e832a4f1
Łukasz Tyczka <lukasz@tyczka.net>
2021-06-26 21:20:42 +0200

    object arrays enum

--- objs-arrays-enum.ts
+++ objs-arrays-enum.ts
@@ -13,31 +13,38 @@
 //     hobbies: ['Sports', 'Reading'],
 //     role: [2, 'author'],
 // };
+
 // const Admin = 0;
 // const Read_Only = 1;
 // const Author = 2;
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 0] = "ADMIN";
-    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 2] = "AUTHOR";
-})(Role || (Role = {}));
-var person = {
+
+enum Role {
+    ADMIN,
+    READ_ONLY,
+    AUTHOR,
+}
+
+const person = {
     name: 'Lukasz',
     age: 28,
     hobbies: ['Sports', 'Reading'],
-    role: Role.ADMIN
+    role: Role.ADMIN,
 };
+
 //person.role.push('admin'); exception !
 // person.role[1] = 10;
+
 //person.role = [0, 'admin', 'user'];
-var favoriteActivities;
+
+let favoriteActivities: string[];
 favoriteActivities = ['Sports'];
+
 console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
+
+for (const hobby of person.hobbies) {
     console.log(hobby.toUpperCase());
 }
+
 if (person.role === Role.ADMIN) {
     console.log('is an Admin');
 }
