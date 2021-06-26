df8dd521547d1a526c48f1028fe4356c7a61971d
Łukasz Tyczka <lukasz@tyczka.net>
2021-06-26 20:57:33 +0200

    finish basic

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-function add(n1, n2) {
-    return n1 + n2;
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
+var person = {
+    name: 'Lukasz',
+    age: 28,
+    hobbies: ['Sports', 'Reading']
+};
+var favoriteActivities;
+favoriteActivities = ['Sports'];
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
 }
-var number1 = 5;
-var number2 = 2.8;
-var result = add(number1, number2);
-console.log(result);
