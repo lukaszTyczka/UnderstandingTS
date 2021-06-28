6ea5115f6d663bfca0a05d3624a10c85cc454a57
Łukasz Tyczka <lukasz@tyczka.net>
2021-06-28 18:42:05 +0200

    functions

--- app.js
+++ app.js
@@ -1,30 +1,18 @@
-// enum conversionType {
-//     AS_NUMBER,
-//     AS_TEXT,
-// }
-function combine(input1, input2, resultConversion) {
-    var result;
-    // not working
-    // if (typeof input1 === typeof input2) {
-    //     result = input1 + input2;
-    // }
-    if ((typeof input1 === 'number' && typeof input2 === 'number') ||
-        resultConversion === 'as-number') {
-        result = +input1 + +input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    // if (resultConversion == 'as_number') {
-    //     return +result;
-    // } else {
-    //     return result.toString();
-    // }
-    return result;
+function add(n1, n2) {
+    return n1 + n2;
 }
-var combinedAges = combine(30, 26, 'as-number');
-console.log(combinedAges);
-var combinedstringAges = combine('30', '26', 'as-number');
-console.log(combinedstringAges);
-var combinedNames = combine('Max', 'Anna', 'as-text');
-console.log(combinedNames);
+function printResult(num) {
+    console.log('Result: ' + num);
+}
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
+printResult(add(5, 12));
+var combineValues;
+combineValues = add;
+// combineValues = printResult;
+console.log(combineValues(8, 8));
+addAndHandle(10, 20, function (r) {
+    console.log(r);
+});
